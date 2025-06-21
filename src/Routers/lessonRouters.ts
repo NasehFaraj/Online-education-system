6551396ef6e3cf347a39330a31bef942af285b7b
Naseh faraj <nasehfara@gmail.com>
2025-06-21 15:59:12 +0300

    fix bugs

--- src/Routers/lessonRouters.ts
+++ src/Routers/lessonRouters.ts
@@ -7,29 +7,29 @@ import { usersMiddleware } from "../Middlewares/usersMiddleware" ;
 
 const router = Router() ;
 
-router.post("/lesson/add" , usersMiddleware([Role.Teacher]) , lessonControlers.addLesson) ;
+router.put("/lesson" , usersMiddleware([Role.Teacher , Role.Admin]) , lessonControlers.addLesson) ;
 
-router.post("/lesson/edit" , usersMiddleware([Role.Teacher]) , lessonControlers.editLesson) ;
+router.patch("/lesson" , usersMiddleware([Role.Teacher , Role.Admin]) , lessonControlers.editLesson) ;
 
-router.post("/lesson/delete" , usersMiddleware([Role.Teacher]) , lessonControlers.deleteLesson) ;
+router.delete("/lesson" , usersMiddleware([Role.Teacher , Role.Admin]) , lessonControlers.deleteLesson) ;
 
-router.post("/lesson/get/all" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.getLessons) ;
+router.get("/lesson" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.getLesson) ;
 
-router.post("/lesson/get" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.getLesson) ;
+router.get("/lesson/all" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.getLessons) ;
 
-router.post("/lesson/get/my" , usersMiddleware([Role.Teacher]) , lessonControlers.getMyLessons) ;
+router.get("/lesson/number-of-lessons" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.getNumberOfLessons) ;
 
-router.post("/lesson/get/my/number-of-lessons" , usersMiddleware([Role.Teacher]) , lessonControlers.getNumberOfMyLessons) ;
+router.get("/lesson/my/all" , usersMiddleware([Role.Teacher , Role.Admin]) , lessonControlers.getMyLessons) ;
 
-router.post("/lesson/get/number-of-lessons" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.getNumberOfLessons) ;
+router.get("/lesson/my/number-of-lessons" , usersMiddleware([Role.Teacher , Role.Admin]) , lessonControlers.getNumberOfMyLessons) ;
 
-router.post("/lesson/library/add" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.addLessonToLibrary) ;
+router.put("/lesson/library" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.addLessonToLibrary) ;
 
-router.post("/lesson/library/delete" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.deleteLessonFromLibrary) ;
+router.delete("/lesson/library" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.deleteLessonFromLibrary) ;
 
-router.post("/lesson/library/get/all" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.getLibrary) ;
+router.get("/lesson/library/all" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.getLibrary) ;
 
-router.post("/lesson/library/number-of-lessons" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.getNumberOfLessonsAtLibrary) ;
+router.get("/lesson/library/number-of-lessons" , usersMiddleware([Role.Teacher , Role.Admin , Role.Student]) , lessonControlers.getNumberOfLessonsAtLibrary) ;
 
 
 export default router ;
